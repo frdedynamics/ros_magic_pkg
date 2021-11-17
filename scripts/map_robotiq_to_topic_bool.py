b7526d20265da590dfbaf0276cabcfc494ce9fd3
gizem <gizemates@iyte.edu.tr>
2021-11-17 10:15:52 +0100

    bool open-close adding

--- scripts/map_robotiq_to_topic_bool.py
+++ scripts/map_robotiq_to_topic_bool.py
@@ -2,23 +2,28 @@
 """ module docstring, yo! """
 import sys
 import rospy
-from std_msgs.msg import Float64
+from std_msgs.msg import Float64, Bool
 from robotiq_urcap_control.msg import Robotiq2FGripper_robot_output as outputMsg
 
 cmd_grip_pub = rospy.Publisher('/Robotiq2FGripperRobotOutput', outputMsg, queue_size=1)
 
 def callback(data):
 
-    msg = outputMsg()
-    msg.rPR = data.data
-    msg.rSP = 10
-    msg.rFR = 10
-    cmd_grip_pub.publish(msg)
+	msg = outputMsg()
+	print data.data
+
+	if data.data:
+		msg.rPR = 0
+	else:
+		msg.rPR = 255
+	msg.rSP = 255
+	msg.rFR = 255
+	cmd_grip_pub.publish(msg)
 
 def listener():
     """ function docstring, yo! """
     rospy.init_node('grab_to_gripper_pub', anonymous=True)
-    rospy.Subscriber("/cmd_grip_pos", Float64, callback)
+    rospy.Subscriber("/cmd_grip", Bool, callback)
     rospy.spin()
 
 
